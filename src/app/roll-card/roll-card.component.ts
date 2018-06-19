1050b555b301290e4b30fa28f610b84aadfc608a
Felipe Schmitz <felipeebs@gmail.com>
2018-06-19 13:48:49 -0300

    Calculates and displays raw sums with a little css

--- src/app/roll-card/roll-card.component.ts
+++ src/app/roll-card/roll-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { RollResult } from '../sandra';
 
 
@@ -7,8 +7,12 @@ import { RollResult } from '../sandra';
   templateUrl: './roll-card.component.html',
   styleUrls: ['./roll-card.component.scss']
 })
-export class RollCardComponent {
+export class RollCardComponent implements OnInit {
   @Input() rollResult: RollResult;
+  total: number;
+  successCount = 0;
+  critCount = 0;
+  failCount = 0;
 
   dieColor(n) {
     if (this.rollResult.isCOmplex()) {
@@ -27,5 +31,24 @@ export class RollCardComponent {
       }
     }
     return 'die-default';
+  }
+
+  ngOnInit() {
+    if (this.rollResult.isCOmplex()) {
+      this.rollResult.roll.forEach(n => {
+        if (n <= this.rollResult.biff) {
+          this.failCount++;
+        }
+        if (n >= this.rollResult.goal) {
+          this.successCount++;
+        }
+        if (n >= this.rollResult.crit) {
+          this.critCount++;
+        }
+      });
+      this.total = this.successCount + this.critCount - this.failCount;
+    } else {
+      this.total = this.rollResult.roll.reduce((s, n) => s + n, 0);
+    }
   }
 }
